1155ee19b13b5d3f5f1a22de692895eaf59bd201
Oppie4246 <oppenheimer4246@protonmail.com>
2024-02-13 12:38:47 +0000

    Fixed buyer/seller typos

--- estate-agent/src/components/Seller/Seller.js
+++ estate-agent/src/components/Seller/Seller.js
@@ -1,4 +1,4 @@
-import React, {useState, useReducer, useEffect} from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import SellerList from "./SellerList";
 import "./Seller.css";
