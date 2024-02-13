17d64438d8f82f86561eb88aad4ce566d1f5e518
Oppie4246 <oppenheimer4246@protonmail.com>
2024-02-13 16:27:19 +0000

    Created property json

--- estate-agent/src/components/Property/PropertyListing.js
+++ estate-agent/src/components/Property/PropertyListing.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function PropertyListing(props) {
+
+
+    const [formData, setForm] = useState({});
+    
+    function submitForm(e){
+        e.preventDefault();
+                 
+            fetch("http://localhost:8000/properties", {
+                method: "POST",
+                headers: {
+                    "content-Type": "application/json",
+                },
+                body: JSON.stringify(formData),
+            })
+            .then((response) => response.json())
+            .then((data) => console.log(data))
+            .catch((error) => console.log(error));
+          
+            
+        for(let key in formData) {
+            sessionStorage.setItem(key, formData[key])
+        };
+    };
 
     return (
         <div className="propertyListing">
