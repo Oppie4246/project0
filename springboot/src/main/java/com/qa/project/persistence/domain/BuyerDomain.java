3b4a4fb4f6a7a836563cfe442ed4dd1388565d63
Oppie4246 <oppenheimer4246@protonmail.com>
2024-03-21 15:18:25 +0000

    Updated

---
+++ springboot/src/main/java/com/qa/project/persistence/domain/BuyerDomain.java
@@ -0,0 +1,10 @@
+package com.qa.project.persistence.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "seller")
+public class SellerDomain {
+
+}
