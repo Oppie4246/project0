58eb281a0dae919637e2c84e079d3df8a97f8399
Oppie4246 <oppenheimer4246@protonmail.com>
2024-03-21 14:23:47 +0000

    Update PropertyDomain.java

--- springboot/src/main/java/com/qa/project/persistence/domain/PropertyDomain.java
+++ springboot/src/main/java/com/qa/project/persistence/domain/PropertyDomain.java
@@ -13,7 +13,6 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
-
 @Entity
 @Table(name = "property")
 public class PropertyDomain {
