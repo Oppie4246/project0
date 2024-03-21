6324d7c4324183c35f942f46da76467f9c6251e2
aname4405 <aname4405@gmail.com>
2024-03-21 14:45:01 +0000

    added booking repo

---
+++ springboot/src/main/java/com/qa/project/persistence/repo/BookingRepo.java
@@ -0,0 +1,6 @@
+package com.qa.project.persistence.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookingRepo extends JpaRepository <BookingRepo, Long> {
+}
