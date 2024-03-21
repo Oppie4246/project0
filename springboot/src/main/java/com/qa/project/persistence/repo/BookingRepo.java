3892ffa213f4e145c592bccaac4ca74e7e66dba3
Oppie4246 <oppenheimer4246@protonmail.com>
2024-03-21 16:07:15 +0000

    Update BookingRepo.java

--- springboot/src/main/java/com/qa/project/persistence/repo/BookingRepo.java
+++ springboot/src/main/java/com/qa/project/persistence/repo/BookingRepo.java
@@ -1,8 +1,9 @@
 package com.qa.project.persistence.repo;
 
+import com.qa.project.persistence.domain.BookingDomain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BookingRepo extends JpaRepository <BookingRepo, Long> {
+public interface BookingRepo extends JpaRepository <BookingDomain, Long> {
 }
