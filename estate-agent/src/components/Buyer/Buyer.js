a9afda054de59bb1f4f4277c8943d669ebd135e3
Oppie4246 <oppenheimer4246@protonmail.com>
2024-04-03 14:18:25 +0100

    update property

--- estate-agent/src/components/Buyer/Buyer.js
+++ estate-agent/src/components/Buyer/Buyer.js
@@ -1,9 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-import banner from '../../assets/banner.png'
-
-
 import styles from "../styles/BuyerDisplay.module.css"
 
 const Buyer = () => {
